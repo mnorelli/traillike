be7cb9f260072da402db7fac30c2b926d1ff6c59
Michael Norelli <mnorelli360@gmail.com>
2016-07-20 16:59:26 -0700

    adds trail with hard-coded trail name, not yet working: fly to center of added trail

--- js/map.js
+++ js/map.js
@@ -5,8 +5,8 @@ window.onload = function(){
   mapObj.mapDraw();
 
   mapObj.addPoint();
-  // mapObj.getTrail("Kirby Cove Trail");
-  // mapObj.addTrail();
+  mapObj.getTrail("Kirby Cove Trail");
+  mapObj.addTrail();
 
   // listener on Google Street View, needs to be added after the rest of the JS loads since it calls mapObj
   panoObj.panorama.addListener('pano_changed', function() {
@@ -19,7 +19,7 @@ window.onload = function(){
 
 // starting location in Marin County, MapBox order = lnglat, Google order = latlng
 // var start_loc_mapbox = [-122.515086,37.841327];  //  Gerbode Valley
-var start_loc_mapbox = [-122.48610019683838, 37.82880236636284]
+var start_loc_mapbox = [-122.538726,37.8507]
 var start_loc_google = switch_coords(start_loc_mapbox,"object");
 var start_zoom = 14;
 
@@ -115,7 +115,7 @@ mapObj.getTrail = function(name){
             "properties": {},
             "geometry": mapObj.trail_geom
           }
-          mapObj.centerPt = turf.center(mapObj.trail_feature);
+          mapObj.centerPt = turf.center(mapObj.trail_geom);
           console.log(mapObj.trail_geom)
         })
         .fail(function() {console.log('error getting trail details'); })
@@ -133,32 +133,7 @@ mapObj.addTrail = function(){
     "data": {
         "type": "Feature",
         "properties": {},
-        "geometry": {
-            "type": "LineString",
-            "coordinates": [
-                [-122.48369693756104, 37.83381888486939],
-                [-122.48348236083984, 37.83317489144141],
-                [-122.48339653015138, 37.83270036637107],
-                [-122.48356819152832, 37.832056363179625],
-                [-122.48404026031496, 37.83114119107971],
-                [-122.48404026031496, 37.83049717427869],
-                [-122.48348236083984, 37.829920943955045],
-                [-122.48356819152832, 37.82954808664175],
-                [-122.48507022857666, 37.82944639795659],
-                [-122.48610019683838, 37.82880236636284],
-                [-122.48695850372314, 37.82931081282506],
-                [-122.48700141906738, 37.83080223556934],
-                [-122.48751640319824, 37.83168351665737],
-                [-122.48803138732912, 37.832158048267786],
-                [-122.48888969421387, 37.83297152392784],
-                [-122.48987674713133, 37.83263257682617],
-                [-122.49043464660643, 37.832937629287755],
-                [-122.49125003814696, 37.832429207817725],
-                [-122.49163627624512, 37.832564787218985],
-                [-122.49223709106445, 37.83337825839438],
-                [-122.49378204345702, 37.83368330777276]
-            ]
-        }
+          "geometry": mapObj.trail_geom
       }
      });
     mapObj.map.addLayer({
@@ -174,7 +149,8 @@ mapObj.addTrail = function(){
             "line-width": 6
         }
     });
-    mapObj.map.flyTo({center: mapObj.centerPt});
+    // mapObj.map.flyTo({center: mapObj.centerPt});
+    // mapObj.map.flyTo({center: start_loc_mapbox});
   });
 };
 
